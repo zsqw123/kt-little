d9d020580ffc10fd32fa75b135dc9d94d6071d04
zsqw123 <zsqw123@vip.qq.com>
2024-03-02 17:11:01 +0800

    done basic list impl

--- src/main/java/com/zsu/immutable/my/MyPersistentList.kt
+++ src/main/java/com/zsu/immutable/my/MyPersistentList.kt
@@ -3,11 +3,14 @@ package com.zsu.immutable.my
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 class MyPersistentList<E>(
     override val size: Int,
-    val allReferences: PersistentList<PersistentList<E>>,
-    val startIndices: PersistentList<Int>, // [startIndices] size always equals with [allReferences]
+    private val allReferences: PersistentList<PersistentList<E>>,
+    private val startIndices: PersistentList<Int>, // [startIndices] size always equals with [allReferences]
 ) : MutateAbstractPersistentList<E>() {
 
     constructor(size: Int, onlyOne: PersistentList<E>) : this(
@@ -18,12 +21,8 @@ class MyPersistentList<E>(
         return MyPersistentListBuilder(size, allReferences, startIndices)
     }
 
-    override fun get(index: Int): E {
-        val refIndex = startIndices.refIndexOf(index)
-        val startIndex = startIndices[refIndex]
-        val ref = allReferences[refIndex]
-        return ref[index - startIndex]
-    }
+    override fun get(index: Int): E =
+        findRef(allReferences, startIndices, index) { _, ref, offset -> ref[offset] }
 
     companion object {
         fun <E> copyOf(originList: List<E>): MyPersistentList<E> {
@@ -41,4 +40,18 @@ fun PersistentList<Int>.refIndexOf(index: Int): Int {
         else low = mid
     }
     return high
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <E, R> findRef(
+    references: PersistentList<PersistentList<E>>,
+    startIndices: PersistentList<Int>, index: Int,
+    onRefFound: (refIndex: Int, ref: PersistentList<E>, offset: Int) -> R
+): R {
+    contract { callsInPlace(onRefFound, InvocationKind.EXACTLY_ONCE) }
+    val refIndex = startIndices.refIndexOf(index)
+    val startIndex = startIndices[refIndex]
+    val ref = references[refIndex]
+    val offset = index - startIndex
+    return onRefFound(refIndex, ref, offset)
 }
--- src/main/java/com/zsu/immutable/my/MyPersistentListBuilder.kt
+++ src/main/java/com/zsu/immutable/my/MyPersistentListBuilder.kt
@@ -2,6 +2,9 @@ package com.zsu.immutable.my
 
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.toPersistentList
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 class MyPersistentListBuilder<E>(
     originSize: Int,
@@ -30,16 +33,14 @@ class MyPersistentListBuilder<E>(
         size += oldSize + elementsSize
 
         val indices = indices
-        val refIndex = indices.refIndexOf(index)
-        val startIndex = indices[refIndex]
-        val ref = references[refIndex]
-        val offset = index - startIndex
-        // change current
-        references = references.set(refIndex, ref.addAll(offset, elements))
+        findRef(index) { refIndex, ref, offset ->
+            // change current
+            references = references.set(refIndex, ref.addAll(offset, elements))
 
-        // make all start indices (which after the current index) plus elementsSize.
-        for (i in (refIndex + 1) until indices.size) {
-            this.indices = this.indices.set(i, indices[i] + elementsSize)
+            // make all start indices (which after the current index) plus elementsSize.
+            for (i in (refIndex + 1) until indices.size) {
+                this.indices = this.indices.set(i, indices[i] + elementsSize)
+            }
         }
 
         return true
@@ -49,35 +50,33 @@ class MyPersistentListBuilder<E>(
         addAll(index, listOf(element))
     }
 
-    override fun get(index: Int): E {
-        val indices = indices
-        val refIndex = indices.refIndexOf(index)
-        val startIndex = indices[refIndex]
-        val ref = references[refIndex]
-        return ref[index - startIndex]
-    }
+    override fun get(index: Int): E =
+        findRef(index) { _, ref, offset -> ref[offset] }
 
-    override fun removeAt(index: Int): E {
-        val indices = indices
-        val refIndex = indices.refIndexOf(index)
-        val startIndex = indices[refIndex]
-        val ref = references[refIndex]
-        val offset = index - startIndex
-
+    override fun removeAt(index: Int): E = findRef(index) { refIndex, ref, offset ->
         // change current
         val removed = ref[offset]
         references = references.set(refIndex, ref.removeAt(offset))
-
         // make all start indices (which after the current index) - 1.
         for (i in (refIndex + 1) until indices.size) {
             this.indices = this.indices.set(i, indices[i] - 1)
         }
-
-        return removed
+        removed
     }
 
-    override fun set(index: Int, element: E): E {
-        TODO("Not yet implemented")
+    override fun set(index: Int, element: E): E = findRef(index) { refIndex, ref, offset ->
+        val origin = ref[offset]
+        references = references.set(refIndex, ref.set(index, element))
+        origin
+    }
+
+    @OptIn(ExperimentalContracts::class)
+    private inline fun <R> findRef(
+        index: Int,
+        onRefFound: (refIndex: Int, ref: PersistentList<E>, offset: Int) -> R
+    ): R {
+        contract { callsInPlace(onRefFound, InvocationKind.EXACTLY_ONCE) }
+        return findRef(references, indices, index, onRefFound)
     }
 
     override fun build(): MyPersistentList<E> {
